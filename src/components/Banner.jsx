aab4b02cb212297bdade597b3a5050f2a9c59a18
Tatiana Mazzeo <mazzeotatiana6@gmail.com>
2025-03-30 12:20:05 -0300

    Fix banner image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,18 +1,21 @@
-import { Link } from 'react-router';
-import './Banner.css'
+import { Link } from "react-router";
+import "./Banner.css";
 import { Image } from "react-bootstrap";
+import bannerPhoto from "../assets/banner.jpg";
 
 function Banner() {
     return (
         <div className="position-relative top-0 banner">
-            <Image src="/src/assets/banner.jpg" className="banner-image" alt="Banner image"></Image>
+            <Image src={bannerPhoto} className="banner-image" alt="Banner image"></Image>
             <div className="overlay"></div>
             <div className="banner-content d-flex flex-column align-items-center">
                 <h1 className="banner-title">Cada bocado es una oportunidad para cuidar de tí mismo</h1>
-                <Link to="/agendarconsulta" className="btn btn-lg mt-4 cta-button">Agendar Consulta</Link>
+                <Link to="/agendarconsulta" className="btn btn-lg mt-4 cta-button">
+                    Agendar Consulta
+                </Link>
             </div>
         </div>
     );
 }
 
-export default Banner
+export default Banner;
